6010d03dbc6814a374bdf7cb5cb90530bd2e3d3e
Samuel Robinson <samueljzrobinson@gmail.com>
2025-02-25 15:20:11 +1000

    Tutorial Credit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// Tutorials Used:
+// Dors Coding School. (2 March 2023). THE ODIN PROJECT: RESTAURANT PAGE | PROJECT SOLUTION. YouTube. https://youtu.be/4V4MRrDhy54?si=plBARbz_MYFClE-l
+
 import { initialLoad } from "./pageLoad";
 
 initialLoad();
